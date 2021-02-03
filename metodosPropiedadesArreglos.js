e41b43af21c7567bccfd5399879f1bf785723bbc
Miles <dufrasnemiles@hotmail.com>
2021-02-03 10:09:44 +0100

    més info

---
+++ metodosPropiedadesArreglos.js
@@ -0,0 +1,63 @@
+//-----
+
+
+
+
+//METODOS Y PROPIEDADES PARA ARREGLOS
+
+var familia = ['Miles', 'Estefania', 'Otto', 'Yuko'];
+var familiaDos = ['Magda', 'Eva']
+
+
+//ES POT POSAR COM A VAR ENCARA QUE NO ES NECESARI ES POT POSAR EL NOM DE LA VAR (familia o familiaDos) y la funcio ex famila.pop o familia.join.....etc.
+
+// lenght // Devuelve la cantidad de elementos del arreglo
+
+
+var cantidadElementosFamilia = familia.length;
+
+// concat // Permite unir arreglos
+
+var nuevaFamilia = familia.concat(familiaDos)
+
+// join // contrario a split, Mostrar todos los elementos del arreglo en una cadena de texto
+
+var texto = familia.join(' - ');
+
+// pop // Eliminar el último elemento del arreglo
+
+// var familiaIncompleta = familia.pop();
+
+// familia.pop();
+
+
+// push // agregar un elemto al final del arreglo
+
+// var familiaMasgrande = familia.push('Karlos');
+
+
+
+// shift // Eliminar el primer elemento del arreglo
+
+// familia.shift();
+
+
+
+// unshift // agregar un elemento al principio del arreglo
+
+
+// familia.unshift('Tomas');
+
+// reverse // ordena el arreglo al reves
+
+
+// familia.reverse();
+
+
+
+
+
+
+
+
+
