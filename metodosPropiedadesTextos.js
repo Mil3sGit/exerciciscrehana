bbf546ba585c68d2f1d37c347bdffebf7a801060
Miles <dufrasnemiles@hotmail.com>
2021-02-03 09:46:31 +0100

    mes exercicis

--- metodosPropiedadesTextos.js
+++ metodosPropiedadesTextos.js
@@ -11,19 +11,45 @@
 //METODOS Y PROPIEDADES PARA CADENAS DE TEXTO
 
 
+var texto = 'Hola, soy un texto';
+
+
+
+
 // lenght - El número de caracteres
+
+var numeroCaracteres = texto.lenght;
 
 // toUpperCase() - Transformar todos lso caracteres a mayúsculas
 
+
+var mayus = texto.toUpperCase();
+
 // toLowerCase() - Transformar los caracteres a minúsculas
 
-// substring(0,0) - Extraer desde un punto hasta otro de caracteres
+var minus = texto.toLowerCase();
+
+
+// substring(0,0) - Extraer desde un punto hasta otro de 
+
+var extraido = texto.substring(0, 7);
 
 // replace(valor1, valor2) - Reemplazar una cadena de caracteres por otra
 
+
+var textoReemplazado = texto.replace('texto', 'niño');
+
 // indexOF('o') - Buscará el primer caracter que coincida e indicará posicion
+
+var buscarO = texto.indexOf('o');
 
 // lastIndexOf('o') - Buscará el último caracter que coincida e indicará posición
 
+
+var buscarUltimaO = texto.lastIndexOf('o');
+
 // split(' ') - Se convierte la cadena de texto en un arreglo dividiendo los elementos según el separador indicado.
+
+var arreglo = texto.split(' ');
+
 
